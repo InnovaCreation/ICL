47f901b88ed7bcb91d727ffe56f7c2f7f98f4059
Cheng Cao <bobcaocheng@163.com>
2017-07-17 21:44:12 +0800

    Store / load profile
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

--- src/lib/Profile.js
+++ src/lib/Profile.js
@@ -3,11 +3,15 @@
 // ==============================================
 // Constructor
 function Profile() {
+	this.profile_name = 'Default'
+
 	this.java_path = '';
 	this.custom_jvm_args = '';
 	this.mc_version_string = '';
 	this.custom_minecraft_args = '';
 	this.native_path = '';
+	
+	this.uuid = uuidv4();
 	
 	this.height = 0;
 	this.width = 0;
@@ -35,14 +39,22 @@ Profile.prototype.GenerateJVMArgs = function(gp) {
 	return args;
 }
 
+// UUID v4 from Chrome developers
+function uuidv4() {
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	)
+}
+
 Profile.prototype.GenerateMinecraftArguments = function(gp) {
 	var args = '--username ' + gp.player_id
 	
 	args += ' --version "ICL 0.X"'
 	args += ' --gameDir "' + $path.join($GameRoot, './.minecraft') + '"'
 	args += ' --assetsDir "' + $path.join($GameRoot, './.minecraft/assets') + '"'
-	args += ' --assetIndex 1.11 --uuid f81253f5efd31d0effe85681d0ab63fc --accessToken f81253f5efd31d0effe85681d0ab63fc --userType Legacy'
+	args += ' --assetIndex 1.11 --uuid ' + this.uuid + ' --accessToken ' + this.uuid;
 	
+	args += ' --userType Legacy'
 	args += ' --versionType "ICL 0.X"';
 	
 	if (this.height > 0) {
@@ -56,6 +68,33 @@ Profile.prototype.GenerateMinecraftArguments = function(gp) {
 	args += this.custom_minecraft_args;
 	
 	return args;
+}
+
+Profile.prototype.StoreJSON = function() {
+	var string = JSON.stringify(this);
+	
+	var fs = require('fs');
+	fs.writeFile(
+		$path.join($GameRoot, this.profile_name + ".profile.json"), 
+		string
+	);
+}
+
+Profile.prototype.LoadFromJSON = function(profile_name) {
+	var json = require($path.join($GameRoot, profile_name + ".profile.json"));
+	
+	this.profile_name = json.profile_name;
+
+	this.java_path = json.java_path;
+	this.custom_jvm_args = json.custom_jvm_args;
+	this.mc_version_string = json.mc_version_string;
+	this.custom_minecraft_args = json.custom_minecraft_args;
+	this.native_path = json.native_path;
+	
+	this.uuid = json.uuid;
+	
+	this.height = json.height;
+	this.width = json.width;
 }
 
 //module.exports.Profile = Profile;
