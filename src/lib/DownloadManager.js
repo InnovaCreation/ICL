62484741e54d5f86eed5bf7ceb744482d300412a
Cheng Cao <bobcaocheng@163.com>
2017-07-19 21:34:35 +0800

    Proper ASYNC download manager that handle files and transimissions properly
    Signed-off-by: Cheng Cao <bobcaocheng@163.com>

---
+++ src/lib/DownloadManager.js
@@ -0,0 +1,84 @@
+var events = require('events');
+
+function DownloadTask(url, to) {
+	this.finished = false;
+	this.emmiter = new events.EventEmitter();
+
+	this.url = url;
+	this.to = to;
+}
+
+var fs = require('fs');
+var path = require('path');
+
+function mkdirRecursiveSync(dirname) {
+	if (!fs.existsSync(dirname)) {
+		mkdirRecursiveSync(path.dirname(dirname));
+		fs.mkdirSync(dirname);
+	}
+}
+
+DownloadTask.prototype.start = function() {
+	var task = this;
+
+	var protocal = task.url.slice(0, task.url.indexOf(':'));
+	if (protocal == 'http' || protocal == 'https') {
+		require(protocal).get(task.url, function(res) {
+			mkdirRecursiveSync(path.dirname(task.to));
+			var file = fs.createWriteStream(task.to);
+			res.on('data', (data) => {file.write(data);});
+
+			res.on('end', function() {
+				file.end();
+				file.on('finish', function(){
+					task.finished = true;
+					task.emmiter.emit('finished', task);
+				})
+			});
+		}).on('error', function(err) {
+			console.log('Error when downloading: ' + err.toString());
+		});
+	}
+
+	return this.emmiter;
+}
+
+module.exports.DownloadTask = DownloadTask;
+
+function DownloadQueue() {
+	this.download_queue = [];
+	this.is_queue_ended = false;
+
+	this.emmiter = new events.EventEmitter();
+}
+
+module.exports.DownloadQueue = DownloadQueue;
+
+DownloadQueue.prototype.add_task = function(url, to) {
+	var queue = this;
+
+	var task = new DownloadTask(url, to);
+	task.start().on('finished', function() {
+		if (queue.is_queue_ended) {
+			var real_end = true;
+			for (i in queue.download_queue) {
+				if (!queue.download_queue[i].finished) {
+					real_end = false;
+					break;
+				}
+			}
+
+			if (real_end) {
+				console.log('Download queue ended');
+				queue.emmiter.emit('finished', queue);
+			}
+		}
+	});
+
+	return task.emmiter;
+}
+
+DownloadQueue.prototype.end_queue = function() {
+	this.is_queue_ended = true;
+	return this.emmiter;
+}
